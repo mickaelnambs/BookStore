62e074d0c04d2cb8ba9e7d0ce70f22d40e398b1c
RANDRIANANTENAINA Nambinintsoa Mickaël <mickael.nambinintsoa07081999@gmail.com>
2024-02-08 23:59:02 +0400

    CRUD book added successfully

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -8,3 +8,61 @@ export const getBooks = async (req, res) => {
         books,
     })
 }
+
+// Create new book => /api/admin/books
+export const newBook = async (req, res) => {
+    const book = await Book.create(req.body);
+
+    res.status(200).json({
+        book
+    });
+}
+
+// Get single book details => /api/books/:id
+export const getBookDetails = async (req, res) => {
+    const book = await Book.findById(req?.params?.id);
+
+    if (!book) {
+        return res.status(404).json({
+            error: "Book not found"
+        });
+    }
+
+    res.status(200).json({
+        book,
+    });
+}
+
+// Update book => /api/admin/books/:id
+export const updateBook = async (req, res) => {
+    let book = await Book.findById(req?.params?.id);
+
+    if (!book) {
+        return res.status(404).json({
+            error: "Book not found"
+        });
+    }
+    
+    book = await Book.findByIdAndUpdate(req?.params?.id, req.body, { new: true });
+
+    res.status(200).json({
+        book,
+    });
+}
+
+// Delete product => /api/admin/books/:id
+export const deleteBook = async (req, res) => {
+    let book = await Book.findById(req?.params?.id);
+
+    if (!book) {
+        return res.status(404).json({
+            error: "Book not found"
+        });
+    }
+
+    await book.deleteOne();
+
+    res.status(200).json({
+        message: "Book deleted",
+    })
+}
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { getBooks } from '../controllers/bookControllers.js';
+import { deleteBook, getBookDetails, getBooks, newBook, updateBook } from '../controllers/bookControllers.js';
 
 const router = express.Router();
 router.route("/books").get(getBooks);
+router.route("/admin/books").post(newBook);
+router.route("/books/:id").get(getBookDetails);
+router.route("/admin/books/:id").put(updateBook);
+router.route("/admin/books/:id").delete(deleteBook);
 
 export default router;
